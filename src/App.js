a09d93d00bc937ae2c244eb03fbba1e79bcadc13
Prasannaa E <53602368+6382737054@users.noreply.github.com>
2024-07-25 08:08:20 +0530

    Deploy latest version of the project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-// src/App.jsx
+// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar'; // Adjust path as needed
-import Footer from './components/Footer'; // Adjust path as needed
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Products from './pages/Products';
-import Gallery from './pages/Gallery'; // Ensure the path is correct
-
+import Gallery from './pages/Gallery';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/COCONUT-WEBSITE">
       <div className="App">
         <Navbar />
         <main className="main-content">
@@ -21,7 +20,7 @@ const App = () => {
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/products" element={<Products />} />
-            <Route path="/gallery" element={<Gallery />} /> {}
+            <Route path="/gallery" element={<Gallery />} />
           </Routes>
         </main>
         <Footer />
